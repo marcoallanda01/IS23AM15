be0dc3f1b8c2d1b5a9298eb96448b0574fd91da0
pietro <pammaturo02@gmailcom>
2023-03-21 19:40:53 +0100

    Specific working, a test for each CommonGoalCard should be added

--- src/main/java/it/polimi/ingsw/modules/Specific.java
+++ src/main/java/it/polimi/ingsw/modules/Specific.java
@@ -48,40 +48,42 @@ public class Specific extends Pattern{
         return (sequence) -> {
             Integer groupNumber = groupNum;
             Boolean sameTypeAcrossGroups = this.sgc;
-            Boolean enoughGroups = sequence.size() > groupNumber;
+            Boolean enoughGroups = sequence.size() >= groupNumber;
             Boolean sameColor = sequence.stream().flatMap(Collection::stream).map(Tile::getType).collect(Collectors.toSet()).size() == 1;
             return enoughGroups && (sameColor || !sameTypeAcrossGroups);
         };
     }
-        // gets all the groups, even if overlapping
+    // gets all the groups, even if overlapping
     private Function<List<List<Optional<Tile>>>, List<List<Tile>>> getAllGroups() {
         return (bookshelf) -> {
             List<List<List<Boolean>>> masks = this.masks; // this.masks
-            int maxTypesInGroup = this.minColor;
-            int minTypesInGroup = this.maxColor;
+            int maxTypesInGroup = this.maxColor;
+            int minTypesInGroup = this.minColor;
             List<List<Tile>> allTheGroups = new ArrayList<>();
-            for (int i = 0; i < bookshelf.size() - masks.get(0).size() - 1; i++) {
-                for (int j = 0; j < bookshelf.get(i).size() - masks.get(0).get(0).size() - 1; j++) {
+            for (int i = 0; i < bookshelf.size() - masks.get(0).size() + 1; i++) {
+                for (int j = 0; j < bookshelf.get(i).size() - masks.get(0).get(0).size() + 1; j++) {
                     // if all masks have been checked, go forward
-                    for (int k = i; k < masks.size(); k++) {
+                    for (int k = 0; k < masks.size(); k++) {
                         List<List<Boolean>> currentMask = masks.get(k);
                         Boolean isPatternValid = true;
                         Set<TileType> typesInPattern = new HashSet<>();
                         List<Tile> validGroup = new ArrayList<>();
-                        for (int l = i; l < currentMask.size() && isPatternValid; l++) {
-                            for (int m = i; m < currentMask.get(l).size() && isPatternValid; m++) {
+                        for (int l = i; l < (currentMask.size() + i) && isPatternValid; l++) {
+                            for (int m = j; m < (currentMask.get(l - i).size() + j) && isPatternValid; m++) {
                                 // if there should be a tile but there isn't the pattern is not valid, change mask
-                                if (currentMask.get(l).get(m) && bookshelf.get(l).get(m).isEmpty()) {
-                                    isPatternValid = false;
-                                } else {
-                                    // if the tile is valid store its type
-                                    typesInPattern.add(bookshelf.get(l).get(m).get().getType());
-                                    // too many types in group
-                                    if (typesInPattern.size() > maxTypesInGroup) {
+                                if (currentMask.get(l - i).get(m - j)) {
+                                    if (bookshelf.get(l).get(m).isEmpty()) {
                                         isPatternValid = false;
                                     } else {
-                                        // store the tile in the group (down here to avoid useless storing)
-                                        validGroup.add(bookshelf.get(l).get(m).get());
+                                        // if the tile is valid store its type
+                                        typesInPattern.add(bookshelf.get(l).get(m).get().getType());
+                                        // too many types in group
+                                        if (typesInPattern.size() > maxTypesInGroup) {
+                                            isPatternValid = false;
+                                        } else {
+                                            // store the tile in the group (down here to avoid useless storing)
+                                            validGroup.add(bookshelf.get(l).get(m).get());
+                                        }
                                     }
                                 }
                             }
@@ -91,7 +93,7 @@ public class Specific extends Pattern{
                             isPatternValid = true;
                         }
                         if (isPatternValid) {
-                            allTheGroups.add(validGroup);
+                            allTheGroups.add(new ArrayList<>(validGroup));
                         }
                     }
                 }
@@ -105,25 +107,32 @@ public class Specific extends Pattern{
     private BiConsumer<List<List<Tile>>, List<List<List<Tile>>>> getPossibleSequencesConsumer() {
         return (startingGroups, notOverlappingSequences) -> {
             BiFunction<List<List<Tile>>, Integer, Boolean> removeOverlappingFunction = this.getRemoveOverlappingFunction();
+            Boolean sequenceChanged = false;
             for (Integer i = 0; i < startingGroups.size(); i++) {
                 List<List<Tile>> toBeFilteredGroups = new ArrayList<>(startingGroups);
                 if (removeOverlappingFunction.apply(toBeFilteredGroups, i)) {
-                    getPossibleSequencesConsumer().accept(startingGroups, notOverlappingSequences);
-                } else {
-                    notOverlappingSequences.add(toBeFilteredGroups);
+                    getPossibleSequencesConsumer().accept(toBeFilteredGroups, notOverlappingSequences);
+                    sequenceChanged = true;
                 }
             }
+            if (!sequenceChanged) {
+                // if nothing changed return the sequence given as parameter
+                notOverlappingSequences.add(startingGroups);
+            }
+
         };
     }
     // removes all groups with any tile overlapping the group at index
     private BiFunction<List<List<Tile>>, Integer, Boolean> getRemoveOverlappingFunction() {
         return (groups, index) -> {
-            List<Tile> mainGroup = groups.get(index);
+            List<Tile> mainGroup = new ArrayList<>(groups.get(index));
             Boolean groupsChanged = false;
-            for (int i = 0; i < mainGroup.size(); i++) {
+            for (Integer i = 0; i < mainGroup.size(); i++) {
                 Tile tile = mainGroup.get(i);
                 List<List<Tile>> overlappingGroups = groups.stream().filter(group -> group.contains(tile)).collect(Collectors.toList());
-                groupsChanged = groupsChanged || groups.removeAll(overlappingGroups);
+                overlappingGroups.remove(mainGroup);
+                Boolean hasRemoved = groups.removeAll(overlappingGroups);
+                groupsChanged = groupsChanged || hasRemoved;
             }
             // returning if any groups have been removed
             return groupsChanged;
---
+++ src/test/java/it/polimi/ingsw/modules/SpecificTest.java
@@ -0,0 +1,144 @@
+package it.polimi.ingsw.modules;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecificTest {
+
+    @Test
+    void testGetPatternFunction() {
+        List<List<List<Boolean>>> masks = new ArrayList<>();
+        masks.add(new ArrayList<>());
+        for (int i = 0; i < 2; i++) {
+            masks.get(0).add(new ArrayList<>());
+            for (int j = 0; j < 2; j++) {
+                masks.get(0).get(i).add(false);
+            }
+        }
+        // one of two
+        masks.get(0).get(0).set(0, true);
+        masks.get(0).get(0).set(1, true);
+        // two of two
+        masks.get(0).get(1).set(0, true);
+        masks.get(0).get(1).set(1, true);
+
+        List<List<Optional<Tile>>> myBookshelf = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            myBookshelf.add(new ArrayList<>());
+            for (int j = 0; j < 6; j++) {
+                myBookshelf.get(i).add(Optional.empty());
+            }
+        }
+        // TCCTT
+        // CCCCT
+        // CCCCT
+        // CCCCC
+        // CCTCC
+        // TTTTT
+        // one of six
+        myBookshelf.get(0).set(0, Optional.of((new Tile(0, 0, TileType.TROPHY))));
+        myBookshelf.get(1).set(0, Optional.of((new Tile(1, 0, TileType.CAT))));
+        myBookshelf.get(2).set(0, Optional.of((new Tile(2, 0, TileType.CAT))));
+        myBookshelf.get(3).set(0, Optional.of((new Tile(3, 0, TileType.TROPHY))));
+        myBookshelf.get(4).set(0, Optional.of((new Tile(4, 0, TileType.TROPHY))));
+        // two of six
+        myBookshelf.get(0).set(1, Optional.of((new Tile(0, 1, TileType.CAT))));
+        myBookshelf.get(1).set(1, Optional.of((new Tile(1, 1, TileType.CAT))));
+        myBookshelf.get(2).set(1, Optional.of((new Tile(2, 1, TileType.CAT))));
+        myBookshelf.get(3).set(1, Optional.of((new Tile(3, 1, TileType.CAT))));
+        myBookshelf.get(4).set(1, Optional.of((new Tile(4, 1, TileType.TROPHY))));
+        // three of six
+        myBookshelf.get(0).set(2, Optional.of((new Tile(0, 2, TileType.CAT))));
+        myBookshelf.get(1).set(2, Optional.of((new Tile(1, 2, TileType.CAT))));
+        myBookshelf.get(2).set(2, Optional.of((new Tile(2, 2, TileType.CAT))));
+        myBookshelf.get(3).set(2, Optional.of((new Tile(3, 2, TileType.CAT))));
+        myBookshelf.get(4).set(2, Optional.of((new Tile(4, 2, TileType.TROPHY))));
+        // four of six
+        myBookshelf.get(0).set(3, Optional.of((new Tile(0, 3, TileType.CAT))));
+        myBookshelf.get(1).set(3, Optional.of((new Tile(1, 3, TileType.CAT))));
+        myBookshelf.get(2).set(3, Optional.of((new Tile(2, 3, TileType.CAT))));
+        myBookshelf.get(3).set(3, Optional.of((new Tile(3, 3, TileType.CAT))));
+        myBookshelf.get(4).set(3, Optional.of((new Tile(4, 3, TileType.CAT))));
+        // five of six
+        myBookshelf.get(0).set(4, Optional.of((new Tile(0, 4, TileType.CAT))));
+        myBookshelf.get(1).set(4, Optional.of((new Tile(1, 4, TileType.CAT))));
+        myBookshelf.get(2).set(4, Optional.of((new Tile(2, 4, TileType.TROPHY))));
+        myBookshelf.get(3).set(4, Optional.of((new Tile(3, 4, TileType.CAT))));
+        myBookshelf.get(4).set(4, Optional.of((new Tile(4, 4, TileType.CAT))));
+        // six of six
+        myBookshelf.get(0).set(5, Optional.of((new Tile(0, 5, TileType.TROPHY))));
+        myBookshelf.get(1).set(5, Optional.of((new Tile(1, 5, TileType.TROPHY))));
+        myBookshelf.get(2).set(5, Optional.of((new Tile(2, 5, TileType.TROPHY))));
+        myBookshelf.get(3).set(5, Optional.of((new Tile(3, 5, TileType.TROPHY))));
+        myBookshelf.get(4).set(5, Optional.of((new Tile(4, 5, TileType.TROPHY))));
+
+        String name = "4squares";
+        Pattern pattern1a = new Specific(name, masks, 4, false, 1, 1);
+        assertEquals(1, pattern1a.getPatternFunction().apply(myBookshelf));
+        Pattern pattern1b = new Specific(name, masks, 5, false, 1, 1);
+        assertEquals(0, pattern1b.getPatternFunction().apply(myBookshelf));
+
+        String name2 = "6squaresWith2Types";
+        Pattern pattern2a = new Specific(name2, masks, 6, false, 1, 2);
+        assertEquals(1, pattern2a.getPatternFunction().apply(myBookshelf));
+        Pattern pattern2b = new Specific(name2, masks, 7, false, 1, 2);
+        assertEquals(0, pattern2b.getPatternFunction().apply(myBookshelf));
+
+        List<List<List<Boolean>>> Xmasks = new ArrayList<>();
+        Xmasks.add(new ArrayList<>());
+        for (int i = 0; i < 3; i++) {
+            Xmasks.get(0).add(new ArrayList<>());
+            for (int j = 0; j < 3; j++) {
+                Xmasks.get(0).get(i).add(false);
+            }
+        }
+        Xmasks.get(0).get(0).set(0, true);
+        Xmasks.get(0).get(0).set(1, false);
+        Xmasks.get(0).get(0).set(2, true);
+
+        Xmasks.get(0).get(1).set(0, false);
+        Xmasks.get(0).get(1).set(1, true);
+        Xmasks.get(0).get(1).set(2, false);
+
+        Xmasks.get(0).get(2).set(0, true);
+        Xmasks.get(0).get(2).set(1, false);
+        Xmasks.get(0).get(2).set(2, true);
+
+        String name3 = "2X";
+        Pattern pattern3a = new Specific(name2, Xmasks, 2, false, 1, 1);
+        assertEquals(1, pattern3a.getPatternFunction().apply(myBookshelf));
+        Pattern pattern3b = new Specific(name2, Xmasks, 4, false, 1, 1);
+        assertEquals(0, pattern3b.getPatternFunction().apply(myBookshelf));
+        String name4 = "4With2Types";
+        Pattern pattern4a = new Specific(name2, Xmasks, 4, false, 1, 2);
+        assertEquals(1, pattern3a.getPatternFunction().apply(myBookshelf));
+        Pattern pattern4b = new Specific(name2, Xmasks, 5, false, 1, 2);
+        assertEquals(0, pattern3b.getPatternFunction().apply(myBookshelf));
+
+        List<List<List<Boolean>>> Cornermasks = new ArrayList<>();
+        Cornermasks.add(new ArrayList<>());
+        for (int i = 0; i < 5; i++) {
+            Cornermasks.get(0).add(new ArrayList<>());
+            for (int j = 0; j < 6; j++) {
+                Cornermasks.get(0).get(i).add(false);
+            }
+        }
+        Cornermasks.get(0).get(0).set(0, true);
+        Cornermasks.get(0).get(0).set(5, true);
+        Cornermasks.get(0).get(4).set(0, true);
+        Cornermasks.get(0).get(4).set(5, true);
+
+
+        String name5 = "Corners";
+        Pattern pattern5a = new Specific(name2, Cornermasks, 1, false, 1, 1);
+        assertEquals(1, pattern5a.getPatternFunction().apply(myBookshelf));
+        Pattern pattern5b = new Specific(name2, Cornermasks, 2, false, 1, 1);
+        assertEquals(0, pattern5b.getPatternFunction().apply(myBookshelf));
+    }
+}
