222a4c933c6ffcaa9df2620fb9e061cb800e3dca
Giuseppe <peppebocci13@gmail.com>
2023-03-23 13:48:33 +0100

    adjustments

--- src/main/java/it/polimi/ingsw/modules/PlayerNotFoundException.java
+++ src/main/java/it/polimi/ingsw/modules/PlayerNotFoundException.java
@@ -4,4 +4,7 @@ public class PlayerNotFoundException extends Exception{
     public PlayerNotFoundException(){
         System.err.println("Player not found");
     }
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
 }
