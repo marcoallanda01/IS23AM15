190a30cf58fd7f396b3779ba5b5adbf1cab90f2f
Luca <luca22.rossi@mail.polimi.it>
2023-04-15 16:57:36 +0200

    Deleted Modules Folder. Updated .gitignore with /saves

--- src/main/java/it/polimi/ingsw/modules/PlayController.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.modules;
-
-public class PlayController {
-    private Game game;
-    public Integer getPlayerPoints(String nickname) throws PlayerNotFoundException {
-        return game.getPlayerPoints(nickname);
-    }
-}
