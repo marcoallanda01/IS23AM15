aa4d5b459e79094499d49c6136f1648a2c54c67b
pietro <pammaturo02@gmailcom>
2023-04-18 20:37:45 +0200

    Adjacent done and tested, EndGamePointsManager done, need more testing

--- src/main/java/it/polimi/ingsw/model/EndGamePointsManager.java
+++ src/main/java/it/polimi/ingsw/model/EndGamePointsManager.java
@@ -2,16 +2,22 @@ package it.polimi.ingsw.model;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public class EndGamePointsManager extends PointsManager{
+    private Set<Pattern> patterns;
     public EndGamePointsManager(List<Player> players, Set<Pattern> patterns) {
         super(players);
     }
 
     public void updatePoints() {}
 
-    public void updatePoints(Player player){}
+    public void updatePoints(Player player){
+        List<List<Optional<Tile>>> bookshelf = player.getBookShelf().getState();
+        Integer newPoints = this.patterns.stream().map(pattern -> pattern.getPatternFunction().apply(bookshelf)).reduce(0, Integer::sum);
+        this.playersToPoints.put(player, newPoints);
+    }
 
     public Set<Pattern> getPatterns() {
         return new HashSet<>();
