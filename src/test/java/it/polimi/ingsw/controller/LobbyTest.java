16b733695eeb794bfe203eb4274f25b2f3d48c28
Giuseppe <peppebocci13@gmail.com>
2023-04-19 03:22:39 +0200

    cacellati test sbagliati

--- src/test/java/it/polimi/ingsw/controller/LobbyTest.java
+++ src/test/java/it/polimi/ingsw/controller/LobbyTest.java
@@ -10,31 +10,10 @@ class LobbyTest {
 
     @Test
     void addFirstPlayer() {
-        Lobby lobby = new Lobby("test");
-        assertFalse(lobby.addFirstPlayer("p1", 5));
-        assertTrue(lobby.addFirstPlayer("p1", 4));
-        assertFalse(lobby.addFirstPlayer("p2", 3));
-
-        lobby = new Lobby("test");
-        assertFalse(lobby.addFirstPlayer("p1", 1));
-        assertTrue(lobby.addFirstPlayer("p1", 2));
     }
 
     @Test
     void addPlayer() {
-        Lobby lobby = new Lobby("test");
-        assertTrue(lobby.addFirstPlayer("p1", 4));
-        try {
-            assertTrue(lobby.addPlayer("p2"));
-        }
-        catch (Exception e){
-            fail();
-        }
-        assertThrows(NicknameTakenException.class, lobby.addPlayer("p2"));
-        assertThrows(NicknameTakenException.class, lobby.addPlayer("p1"));
-        assertTrue(lobby.addPlayer("p3"));
-        assertTrue(lobby.addPlayer("p4"));
-        assertThrows(FullGameException.class, lobby.addPlayer("p5"));
     }
 
     @Test
