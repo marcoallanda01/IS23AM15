c1e797a3c34c505cdc2529c8c204968a8b678f4c
pietro <pammaturo02@gmailcom>
2023-05-08 00:59:05 +0200

    added test case for 0 right tiles, 100% line and branch coverage an all patterns

--- src/test/java/it/polimi/ingsw/server/model/PersonalPatternTest.java
+++ src/test/java/it/polimi/ingsw/server/model/PersonalPatternTest.java
@@ -56,6 +56,50 @@ class PersonalPatternTest {
         assertEquals(12, pattern.getPatternFunction().apply(bookshelfState));
     }
     @Test
+    void test0Right() throws InvalidPatternParameterException {
+        // | - | - | - | - | G |
+        // | - | - | - | - | - |
+        // | - | T | - | - | - |
+        // | - | - | T | - | - |
+        // | G | - | - | - | - |
+        // | - | P | - | F | - |
+        List<Tile> checks =
+                List.of(new Tile(0,1,TileType.GAME), new Tile(1, 0, TileType.PLANT),
+                        new Tile(1, 3, TileType.TROPHY), new Tile(2,2,TileType.TROPHY),
+                        new Tile(3,0,TileType.FRAME), new Tile(4,5,TileType.GAME));
+        ArrayList<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 1});
+        list.add(new int[]{2, 2});
+        list.add(new int[]{3, 4});
+        list.add(new int[]{4, 6});
+        list.add(new int[]{5, 9});
+        list.add(new int[]{6, 12});
+
+        // | - | - | - | C | P |
+        // | - | - | - | F | B |
+        // | - | B | - | T | B |
+        // | - | G | B | T | B |
+        // | - | B | P | C | B |
+        // | - | C | C | B | P |
+        BookShelf bookShelf = new BookShelf();
+        // Inserting tiles to match the pattern
+        List<Tile> firstRow = List.of(new Tile(TileType.GAME));
+        bookShelf.insertTiles(firstRow, 0);
+        List<Tile> secondRow = List.of(new Tile(TileType.CAT), new Tile(TileType.BOOK), new Tile(TileType.GAME), new Tile(TileType.BOOK));
+        bookShelf.insertTiles(secondRow, 1);
+        List<Tile> thirdRow = List.of(new Tile(TileType.CAT), new Tile(TileType.PLANT), new Tile(TileType.BOOK));
+        bookShelf.insertTiles(thirdRow, 2);
+        List<Tile> fourthRow = List.of(new Tile(TileType.BOOK), new Tile(TileType.CAT), new Tile(TileType.TROPHY), new Tile(TileType.TROPHY), new Tile(TileType.FRAME), new Tile(TileType.CAT));
+        bookShelf.insertTiles(fourthRow, 3);
+        List<Tile> fifthRow = List.of(new Tile(TileType.PLANT), new Tile(TileType.BOOK), new Tile(TileType.BOOK), new Tile(TileType.BOOK), new Tile(TileType.BOOK), new Tile(TileType.PLANT));
+        bookShelf.insertTiles(fifthRow, 4);
+        List<List<Optional<Tile>>> bookshelfState = bookShelf.getState();
+
+        String name = "P";
+        Pattern pattern = new PersonalPattern(name, checks, list);
+        assertEquals(0, pattern.getPatternFunction().apply(bookshelfState));
+    }
+    @Test
     void test1Right() throws InvalidPatternParameterException {
         // | - | - | - | - | G |
         // | - | - | - | - | - |
