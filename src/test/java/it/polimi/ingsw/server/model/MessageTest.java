2607fdab3dabf1412929fc342117394abff86821
marcoallanda <marco.allanda@mail.polimi.it>
2023-05-29 14:24:44 +0200

    Chat and message test fixed

---
+++ src/test/java/it/polimi/ingsw/server/model/MessageTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.server.model;
+
+import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.chat.Message;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+    @Test
+    void getContent() {
+        Message m = new Message(new Player("sender"), new Player("receiver"), "ciao");
+        assertEquals(m.getContent(), m.getContent());
+    }
+
+    @Test
+    void getSenderName() {
+        Message m = new Message(new Player("sender"), new Player("receiver"), "ciao");
+        assertEquals(m.getSenderName(), m.getSender().getUserName());
+    }
+
+    @Test
+    void getDate() {
+        Message m = new Message(new Player("sender"), new Player("receiver"), "ciao");
+        assertEquals(m.getDate(), m.getDate().toString());
+    }
+}
